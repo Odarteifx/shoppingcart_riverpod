24f8d1b9c67109a654da28d2ae13e9feadc8f99c
william lamptey <williamlamp512@gmail.com>
2024-07-18 22:20:35 +0000

    Best Pick Ui designed

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -5,7 +5,7 @@ List<Product> allProducts = [
  Product(id: 2, productname: 'Shawarma', price: 69.00, image: 'assets/shawarma.png'),
  Product(id: 3, productname: 'Burger', price: 120.00, image: 'assets/burger.png'),
  Product(id: 4, productname: 'Chicken Nuggets', price: 65.00, image: 'assets/chicken_nuggets.png'),
- Product(id: 5, productname: 'French Fries', price: 40.00, image: 'assets/coke.png'),
+ Product(id: 5, productname: 'French Fries', price: 40.00, image: 'assets/french_fries.png'),
  Product(id: 6, productname: 'Chicken Wings', price: 85.00, image: 'assets/chicken_wings.png'),
  Product(id: 7, productname: 'Coca Cola', price: 15.00, image: 'assets/coke.png'),
  Product(id: 8, productname: 'Bottle Water', price: 3.50, image: 'assets/bottle_water.png')
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shoppingcart_riverpod/riverpod/product_riverpord.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -21,11 +23,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FoodPage extends StatelessWidget {
+class FoodPage extends ConsumerWidget {
   const FoodPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allProducts = ref.watch(productsProvder);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Best Pick'),
@@ -37,21 +40,62 @@ class FoodPage extends StatelessWidget {
       ),
       body: GridView.builder(
         padding: const EdgeInsets.all(20),
-        itemCount: 8,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 20,
-              crossAxisSpacing: 20,
-              childAspectRatio: 0.8,
-              ),
+        itemCount: allProducts.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          childAspectRatio: 0.8,
+        ),
         itemBuilder: (context, index) {
-          return Container(
-            height: 200,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primaryContainer.withOpacity(0.4),
-              borderRadius: BorderRadius.circular(15),
-            ),
+          return Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Theme.of(context)
+                      .colorScheme
+                      .primaryContainer
+                      .withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      height: 120,
+                      allProducts[index].image,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      allProducts[index].productname,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text('GH₵${allProducts[index].price.toStringAsFixed(2)}')
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 165,
+                left: 125,
+                  child: Center(
+                    child: IconButton.filled(
+                      padding: const EdgeInsets.all(1),
+                      visualDensity: const VisualDensity(horizontal: -2.5, vertical: -2.5),
+                        onPressed: () {}, icon: const Icon(
+                          Iconsax.shopping_bag,
+                          size: 15,
+                          )),
+                  ))
+            ],
           );
         },
       ),
