1b9f70886782c15c324441fc7c5b8d5265ef11b3
william lamptey <williamlamp512@gmail.com>
2024-07-18 19:59:59 +0000

    Built gridview for products

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
+      ),
+      home: const FoodPage(),
+    );
+  }
+}
+
+class FoodPage extends StatelessWidget {
+  const FoodPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Best Pick'),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Badge(child: Icon(Icons.shopping_bag_outlined)))
+        ],
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(20),
+        itemCount: 8,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              childAspectRatio: 0.8,
+              ),
+        itemBuilder: (context, index) {
+          return Container(
+            height: 200,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer.withOpacity(0.4),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
